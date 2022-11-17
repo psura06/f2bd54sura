d3323887c43bd305196d24676a6933b1610e0612
Pooja Sura <S555254@nwmissouri.edu>
2022-11-16 19:04:43 -0600

    Added Styles

--- routes/Drinks.js
+++ routes/Drinks.js
@@ -1,9 +1,21 @@
 var express = require('express');
+const Drinks_controlers= require('../controllers/Drinks');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('Drinks', { title: 'Pooja Sura Search Results' });
-});
+/* GET Drinks */
+
+router.get('/', Drinks_controlers.Drinks_view_all_Page );
+
+/* GET detail Drinks page */ 
+router.get('/detail', Drinks_controlers.Drinks_view_one_Page);
+
+/* GET create Drinks page */ 
+router.get('/create', Drinks_controlers.Drinks_create_Page); 
+
+/* GET create update page */ 
+router.get('/update', Drinks_controlers.Drinks_update_Page); 
+
+/* GET delete DrinKS page */ 
+router.get('/delete', Drinks_controlers.Drinks_delete_Page); 
 
 module.exports = router;
